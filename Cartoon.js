10036eefcaad2b64cc382f0a42249555085e2ad8
Milan Sharma <130557746+Milan-Sharma1@users.noreply.github.com>
2024-09-23 22:34:13 +0530

    Update Cartoon.js

--- Cartoon.js
+++ Cartoon.js
@@ -4,42 +4,42 @@ for(var i=0;i<noofpics;i++){
         var buttoninnerHTML=this.id;
         switch(buttoninnerHTML){
             case "btn1":
-                var shinchan=new Audio("./Audios/Shinchan.mp3");
+                var shinchan=new Audio("./Shinchan.mp3");
                 shinchan.play();
                 setTimeout(()=> {
                     shinchan.pause();
                 }, 4000)
                 break;
             case "btn2":
-                var doraemon=new Audio("./Audios/Doraemon.mp3");
+                var doraemon=new Audio("./Doraemon.mp3");
                 doraemon.play();
                 setTimeout(()=> {
                     doraemon.pause();
                 }, 5000)
                 break;
             case "btn3":
-                var nobita=new Audio("./Audios/nobita.mp3");
+                var nobita=new Audio("./nobita.mp3");
                 nobita.play();
                 setTimeout(()=> {
                     nobita.pause();
                 }, 6000)
                 break;
             case "btn4":
-                var chotabheem=new Audio("./Audios/ChotaBheem.mp3");
+                var chotabheem=new Audio("./ChotaBheem.mp3");
                 chotabheem.play();
                 setTimeout(()=> {
                     chotabheem.pause();
                 }, 4000)
                 break;
             case "btn5":
-                var ninja=new Audio("./Audios/Ninjahattori.mp3");
+                var ninja=new Audio("./Ninjahattori.mp3");
                 ninja.play();
                 setTimeout(()=> {
                     ninja.pause();
                 }, 5000)
                 break;
             case "btn6":
-                var pockemon=new Audio("./Audios/pikachu.mp3");
+                var pockemon=new Audio("./pikachu.mp3");
                 pockemon.play();
                 setTimeout(()=> {
                     pockemon.pause();
